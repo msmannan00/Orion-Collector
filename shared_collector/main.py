e4fb42995c5addf4f807c08546a8f76558a291da
Muhammad Hassan Arshad <152703576+MHassanAr@users.noreply.github.com>
2025-02-12 00:17:47 +0500

    Crawl flock website

--- shared_collector/main.py
+++ shared_collector/main.py
@@ -1,10 +1,10 @@
 from crawler.request_manager import check_services_status, parse_leak_data
-from shared_collector.scripts._weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd import _weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd
-from shared_collector.scripts._zone_xsec import _zone_xsec
+from shared_collector.scripts._flock4cvoeqm4c62gyohvmncx6ck2e7ugvyqgyxqtrumklhd5ptwzpqd import \
+    _flock4cvoeqm4c62gyohvmncx6ck2e7ugvyqgyxqtrumklhd5ptwzpqd
 
 check_services_status()
 
 if __name__ == "__main__":
-    parse_sample = _weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd()
-    parsed_data, raw_parse_mapping = parse_leak_data(blocked_media=False, proxy={"server": "socks5://127.0.0.1:9150"}, model=parse_sample)
+    parse_sample = _flock4cvoeqm4c62gyohvmncx6ck2e7ugvyqgyxqtrumklhd5ptwzpqd()
+    parsed_data, raw_parse_mapping = parse_leak_data(blocked_media=True, proxy={"server": "socks5://127.0.0.1:9150"}, model=parse_sample)
     print(parsed_data)
