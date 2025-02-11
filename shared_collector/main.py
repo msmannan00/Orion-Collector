0055aa681a720e59e818e085a45479f06a0b8c01
عبدالمنان <msmannan00@protonmail.com>
2025-02-11 12:30:51 +0500

    bug fixes

--- shared_collector/main.py
+++ shared_collector/main.py
@@ -1,10 +1,13 @@
 from crawler.request_manager import check_services_status, parse_leak_data
+from shared_collector.scripts._3ev4metjirohtdpshsqlkrqcmxq6zu3d7obrdhglpy5jpbr7whmlfgqd import _3ev4metjirohtdpshsqlkrqcmxq6zu3d7obrdhglpy5jpbr7whmlfgqd
+from shared_collector.scripts._ebhmkoohccl45qesdbvrjqtyro2hmhkmh6vkyfyjjzfllm3ix72aqaid import _ebhmkoohccl45qesdbvrjqtyro2hmhkmh6vkyfyjjzfllm3ix72aqaid
+from shared_collector.scripts._handala_hack import _handala_hack
 from shared_collector.scripts._weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd import _weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd
 from shared_collector.scripts._zone_xsec import _zone_xsec
 
 check_services_status()
 
 if __name__ == "__main__":
-    parse_sample = _weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd()
-    parsed_data, raw_parse_mapping = parse_leak_data(blocked_media=False, proxy={"server": "socks5://127.0.0.1:9150"}, model=parse_sample)
+    parse_sample = _ebhmkoohccl45qesdbvrjqtyro2hmhkmh6vkyfyjjzfllm3ix72aqaid()
+    parsed_data, raw_parse_mapping = parse_leak_data(blocked_media=True, proxy={"server": "socks5://127.0.0.1:9150"}, model=parse_sample)
     print(parsed_data)
