08bc4013fff15ddf838dcc2321e68dcf84265a2f
Usman Ali <168332899+usmancout@users.noreply.github.com>
2025-03-05 12:00:41 +0500

    Update _vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd.py

--- shared_collector/scripts/_vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd.py
+++ shared_collector/scripts/_vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd.py
@@ -57,22 +57,13 @@ class _vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd(leak_extractor_i
         return "https://www.iana.org/help/example-domains"
 
     def parse_leak_data(self, page: Page):
+        page.wait_for_selector('card-body')
+        title_elements = page.query_selector_all('div.card-body.p-3.pt-2 a.h5')
 
-        soup = BeautifulSoup(page.content(), 'html.parser')
-        card_body_divs = soup.find_all('div', class_='card-body')
+        title_urls_list = [element.get_attribute('href') for element in title_elements]
 
-        download_links = []
-
-        for div in card_body_divs:
-            # Find download buttons specifically
-            download_buttons = div.find_all('a',
-                                            class_='app-academy-md-50 btn btn-label-primary d-flex align-items-center',
-                                            href=True)
-            links = [a['href'] for a in download_buttons]
-            download_links.extend(links)
-
-        print(download_links)
-        for link_url in download_links:
+        print(title_urls_list)
+        for link_url in title_urls_list:
             page.goto(link_url)
 
             content_element = page.query_selector('.card-body.ql-editor')
@@ -84,7 +75,7 @@ class _vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd(leak_extractor_i
             link_elements = page.query_selector_all('a[href]')
             web_links = [a.get_attribute('href') for a in link_elements]
 
-            card_data = card_extraction_model(
+            self._card_data = card_extraction_model(
                 m_title=page.title(),
                 m_url=page.url,
                 m_base_url=self.base_url,
