b8b3a273d1d8e792486b4c80fe3b04108c84bfd6
عبدالمنان <msmannan00@protonmail.com>
2025-03-20 19:08:38 +0500

    bug fixes

--- leak_collector/main.py
+++ leak_collector/main.py
@@ -1,9 +1,9 @@
 from crawler.request_manager import check_services_status, parse_leak_data
-from leak_collector.scripts._monitor_mozilla import _monitor_mozilla
+from leak_collector._example import _example
 
 check_services_status()
 
 if __name__ == "__main__":
-    parse_sample = _monitor_mozilla()
+    parse_sample = _example()
     parsed_data, raw_parse_mapping = parse_leak_data(proxy={"server": "socks5://127.0.0.1:9150"}, model=parse_sample)
     print(parsed_data)
