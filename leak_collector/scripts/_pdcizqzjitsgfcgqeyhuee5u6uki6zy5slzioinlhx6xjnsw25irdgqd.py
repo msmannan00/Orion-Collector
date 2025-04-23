ed3735ce5068f4407261a6d17b4aaf0e59f4b1fd
Muhammad Hassan Arshad <152703576+MHassanAr@users.noreply.github.com>
2025-04-23 15:58:29 +0500

    Create _pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd.py

---
+++ leak_collector/scripts/_pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd.py
@@ -0,0 +1,107 @@
+from abc import ABC
+from typing import List
+from playwright.sync_api import Page
+from crawler.crawler_instance.local_interface_model.leak.leak_extractor_interface import leak_extractor_interface
+from crawler.crawler_instance.local_shared_model.data_model.entity_model import entity_model
+from crawler.crawler_instance.local_shared_model.data_model.leak_model import leak_model
+from crawler.crawler_instance.local_shared_model.rule_model import RuleModel, FetchProxy, FetchConfig
+from crawler.crawler_services.redis_manager.redis_controller import redis_controller
+from crawler.crawler_services.redis_manager.redis_enums import REDIS_COMMANDS, CUSTOM_SCRIPT_REDIS_KEYS
+from crawler.crawler_services.shared.helper_method import helper_method
+from datetime import datetime
+
+class _pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd(leak_extractor_interface, ABC):
+    _instance = None
+
+    def __init__(self, callback=None):
+        self.callback = callback
+        self._card_data = []
+        self._entity_data = []
+        self.soup = None
+        self._initialized = None
+        self._redis_instance = redis_controller()
+
+    def init_callback(self, callback=None):
+        self.callback = callback
+
+    def __new__(cls, callback=None):
+        if cls._instance is None:
+            cls._instance = super(_pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd, cls).__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
+    @property
+    def seed_url(self) -> str:
+        return "http://pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd.onion"
+
+    @property
+    def base_url(self) -> str:
+        return "http://pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd.onion"
+
+    @property
+    def rule_config(self) -> RuleModel:
+        return RuleModel(m_fetch_proxy=FetchProxy.TOR, m_fetch_config=FetchConfig.PLAYRIGHT)
+
+    @property
+    def card_data(self) -> List[leak_model]:
+        return self._card_data
+
+    @property
+    def entity_data(self) -> List[entity_model]:
+        return self._entity_data
+
+    def invoke_db(self, command: REDIS_COMMANDS, key: CUSTOM_SCRIPT_REDIS_KEYS, default_value):
+        return self._redis_instance.invoke_trigger(command, [key.value + self.__class__.__name__, default_value])
+
+    def contact_page(self) -> str:
+        return "http://pdcizqzjitsgfcgqeyhuee5u6uki6zy5slzioinlhx6xjnsw25irdgqd.onion/Contact-Us.html"
+
+    def append_leak_data(self, leak: leak_model, entity: entity_model):
+        self._card_data.append(leak)
+        self._entity_data.append(entity)
+        if self.callback:
+            self.callback()
+
+    def parse_leak_data(self, page: Page):
+        try:
+            cards = page.query_selector_all('.post-card')
+
+            for card in cards:
+                title_el = card.query_selector("h4")
+                date_el = card.query_selector(".date")
+                description_el = card.query_selector(".subtitle")
+                size_el = card.query_selector(".size")
+
+                title = title_el.text_content().strip() if title_el else "No Title"
+                date_str = date_el.text_content().strip() if date_el else None
+                description = description_el.text_content().strip() if description_el else "No Description"
+                size = size_el.text_content().strip() if size_el else "No Size"
+
+                leak_date = None
+                if date_str:
+                    leak_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+
+                card_data = leak_model(
+                    m_title=title,
+                    m_url=page.url,
+                    m_base_url=self.base_url,
+                    m_screenshot=helper_method.get_screenshot_base64(page, title),
+                    m_content=description,
+                    m_network=helper_method.get_network_type(self.base_url),
+                    m_important_content=description,
+                    m_content_type=["leaks"],
+                    m_leak_date=leak_date,
+                    m_data_size=size,
+                )
+
+                entity_data = entity_model(
+                    m_email_addresses=helper_method.extract_emails(description),
+                    m_phone_numbers=helper_method.extract_phone_numbers(description),
+                )
+
+                self.append_leak_data(card_data, entity_data)
+
+        except Exception as e:
+            print(f"An error occurred while parsing leak data: {e}")
+
+
