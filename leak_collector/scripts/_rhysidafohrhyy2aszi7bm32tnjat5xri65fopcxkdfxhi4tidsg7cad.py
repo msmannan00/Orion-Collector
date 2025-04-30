21d9c457274175b8cc5d407d831209da8cb0525d
Usman Ali <168332899+usmancout@users.noreply.github.com>
2025-04-30 10:44:47 +0500

    Update _rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad.py

--- leak_collector/scripts/_rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad.py
+++ leak_collector/scripts/_rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad.py
@@ -68,25 +68,72 @@ class _rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad(leak_extractor_i
             self.callback()
 
     def parse_leak_data(self, page: Page):
-        
-        m_content = ""
 
-        card_data = leak_model(
-            m_title=page.title(),
-            m_url=page.url,
-            m_base_url=self.base_url,
-            m_screenshot="",
-            m_content=m_content,
-            m_network=helper_method.get_network_type(self.base_url),
-            m_important_content=m_content,
-            m_weblink=[],
-            m_dumplink=[],
-            m_content_type=["leaks"],
-        )
+        page.wait_for_selector("button#archive_button")
 
-        entity_data = entity_model(
-            m_email_addresses=helper_method.extract_emails(m_content),
-            m_phone_numbers=helper_method.extract_phone_numbers(m_content),
-        )
+        page.click("button#archive_button")
+        page.wait_for_timeout(2000)
 
-        self.append_leak_data(card_data, entity_data)
+        more_buttons = page.locator("button.btn.btn-secondary[data-company]")
+        button_count = more_buttons.count()
+
+
+        for i in range(button_count):
+            more_buttons = page.locator("button.btn.btn-secondary[data-company]")
+            button = more_buttons.nth(i)
+            button.click()
+            page.wait_for_timeout(1500)
+
+            title = page.locator("div.col-8 p.h4").first.text_content() or ""
+
+            p_tags = page.locator("div.col-8 p").all()
+            descriptions = []
+            for p in p_tags:
+                text = p.text_content()
+                if text and "Documents" not in text:
+                    descriptions.append(text)
+            description = "\n".join(descriptions)
+
+            doc_links = []
+            a_tags = page.locator("div.col-8 a").all()
+            for a in a_tags:
+                href = a.get_attribute("href")
+                if href and ".onion" in href:
+                    doc_links.append(href)
+
+            img_urls = []
+            images = page.locator("div.row.m-2 div.col img").all()
+            for img in images:
+                src = img.get_attribute("src")
+                if src:
+                    img_urls.append(src)
+
+            extra_description = page.locator("div.m-2").nth(-1).text_content() or ""
+
+            m_content = f"{description}\n\nDocuments:\n" + "\n".join(doc_links) + "\n\nImages:\n" + "\n".join(
+                img_urls) + f"\n\nAdditional Info:\n{extra_description}"
+
+            card_data = leak_model(
+                m_title=title,
+                m_url=page.url,
+                m_base_url=self.base_url,
+                m_screenshot="",
+                m_content=m_content,
+                m_network=helper_method.get_network_type(self.base_url),
+                m_important_content=m_content,
+                m_weblink=doc_links,
+                m_dumplink=[],
+                m_content_type=["leaks"],
+            )
+
+            entity_data = entity_model(
+                m_email_addresses=helper_method.extract_emails(m_content),
+                m_phone_numbers=helper_method.extract_phone_numbers(m_content),
+            )
+
+            self.append_leak_data(card_data, entity_data)
+
+            page.keyboard.press("Escape")
+            page.wait_for_timeout(1000)
+
+
