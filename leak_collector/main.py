ecc27df5324f4221bc00acdb2c6a8e51580aaf1e
عبدالمنان <msmannan00@protonmail.com>
2025-05-01 20:16:36 +0500

    Update main.py

--- leak_collector/main.py
+++ leak_collector/main.py
@@ -1,14 +1,10 @@
 from crawler.request_manager import check_services_status
 from crawler.request_parser import RequestParser
-from leak_collector.scripts._ddosecrets import _ddosecrets
-from leak_collector.scripts._lynxblogco7r37jt7p5wrmfxzqze7ghxw6rihzkqc455qluacwotciyd import _lynxblogco7r37jt7p5wrmfxzqze7ghxw6rihzkqc455qluacwotciyd
-from leak_collector.scripts._monitor_mozilla import _monitor_mozilla
-from leak_collector.scripts._weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd import _weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd
-from telegram_collector.scripts._telegram_extractor import _telegram_extractor
+from leak_collector._example import _example
 
 check_services_status()
 
 if __name__ == "__main__":
-    parse_sample = _monitor_mozilla()
+    parse_sample = _example()
     parser = RequestParser(proxy={"server": "socks5://127.0.0.1:9150"}, model=parse_sample).parse()
     print(parse_sample.card_data)
