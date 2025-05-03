855bc7f406d8656c0cb357167de7d8fc8fda98ae
Usman Ali <168332899+usmancout@users.noreply.github.com>
2025-05-03 23:02:32 +0500

    Update _ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad_update.py

--- leak_collector/scripts/_ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad_update.py
+++ leak_collector/scripts/_ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad_update.py
@@ -8,7 +8,8 @@ from crawler.crawler_instance.local_shared_model.rule_model import RuleModel, Fe
 from crawler.crawler_services.redis_manager.redis_controller import redis_controller
 from crawler.crawler_services.redis_manager.redis_enums import REDIS_COMMANDS, CUSTOM_SCRIPT_REDIS_KEYS
 from crawler.crawler_services.shared.helper_method import helper_method
-
+from urllib.parse import urlparse
+import time
 
 class _ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad(leak_extractor_interface, ABC):
     _instance = None
@@ -39,7 +40,7 @@ class _ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad(leak_extractor_i
 
     @property
     def base_url(self) -> str:
-        return "http://ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad.onion/news"
+        return "http://ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad.onion"
 
     @property
     def rule_config(self) -> RuleModel:
@@ -69,27 +70,57 @@ class _ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad(leak_extractor_i
             self.callback()
 
 
-
     def parse_leak_data(self, page: Page):
+        page.wait_for_selector('div.category-item.js-open-chat')
 
-        m_content = ""
+        category_items = page.query_selector_all('div.category-item.js-open-chat')
+        link_urls = []
+        for item in category_items:
+            translit = item.get_attribute('data-translit')
+            if translit:
+                full_url = f"{self.seed_url}/{translit}"
+                link_urls.append(full_url)
 
-        card_data = leak_model(
-            m_title=page.title(),
-            m_url=page.url,
-            m_base_url=self.base_url,
-            m_screenshot="",
-            m_content=m_content,
-            m_network=helper_method.get_network_type(self.base_url),
-            m_important_content=m_content,
-            m_weblink=[],
-            m_dumplink=[],
-            m_content_type=["leaks"],
-        )
+        for url in link_urls:
+            page.goto(url)
+            page.wait_for_load_state("networkidle")
+            title = page.query_selector('div.flex.js-c-sb h3').inner_text() if page.query_selector(
+                'div.flex.js-c-sb h3') else ""
+            description = page.query_selector('p.mt-3.publication-description').inner_text() if page.query_selector(
+                'p.mt-3.publication-description') else ""
+            date = page.query_selector('div.mt-3.date-view').inner_text() if page.query_selector(
+                'div.mt-3.date-view') else ""
+            views = page.query_selector('div.mt-3.count-view.mr-5').inner_text().strip() if page.query_selector(
+                'div.mt-3.count-view.mr-5') else ""
 
-        entity_data = entity_model(
-            m_email_addresses=helper_method.extract_emails(m_content),
-            m_phone_numbers=helper_method.extract_phone_numbers(m_content),
-        )
+            content = f"{title}: {description} Date: {date} Views: {views}"
+            important_content = description
 
-        self.append_leak_data(card_data, entity_data)
+            image_elements = page.query_selector_all('div a.form-image-preview img')
+            images = []
+            for img in image_elements:
+                img_src = img.get_attribute('src')
+                if img_src:
+                    parsed_url = urlparse(img_src)
+                    images.append(parsed_url.path.strip())
+
+            card_data = leak_model(
+                m_screenshot="",
+                m_title=title,
+                m_url=url,
+                m_base_url=self.base_url,
+                m_content=content,
+                m_network=helper_method.get_network_type(self.base_url),
+                m_important_content=important_content,
+                m_dumplink=[],  # No explicit download link provided in this case
+                m_content_type=["leaks"],
+                m_logo_or_images=images,
+                m_weblink=[],
+            )
+
+            entity_data = entity_model(
+                m_email_addresses=helper_method.extract_emails(content),
+                m_phone_numbers=helper_method.extract_phone_numbers(content),
+            )
+
+            self.append_leak_data(card_data, entity_data)
