8e73c450198157ab470ac7deb960520022e370be
Usman Ali <168332899+usmancout@users.noreply.github.com>
2025-05-03 06:08:00 +0500

    Update _vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd_update.py

--- leak_collector/scripts/_vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd_update.py
+++ leak_collector/scripts/_vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd_update.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from typing import List
 from playwright.sync_api import Page
-
+from bs4 import BeautifulSoup
 from crawler.crawler_instance.local_interface_model.leak.leak_extractor_interface import leak_extractor_interface
 from crawler.crawler_instance.local_shared_model.data_model.entity_model import entity_model
 from crawler.crawler_instance.local_shared_model.data_model.leak_model import leak_model
@@ -75,30 +75,45 @@ class _vkvsgl7lhipjirmz6j5ubp3w3bwvxgcdbpi3fsbqngfynetqtw4w5hyd(leak_extractor_i
         title_elements = page.query_selector_all('div.card-body.p-3.pt-2 a.h5')
         title_urls_list = [element.get_attribute('href') for element in title_elements]
 
-        print(title_urls_list)
         for link_url in title_urls_list:
             page.goto(link_url)
 
-            content_element = page.query_selector('.card-body.ql-editor')
-            m_content = content_element.inner_text().strip() if content_element else ""
+            m_content = page.inner_text("html")
+            unwanted_terms = ["Brain", "Cipher", "Main", "FAQ", "Rules", "Download"]
+            m_content = " ".join(
+                word for word in m_content.split() if word not in unwanted_terms
+            )
 
-            first_paragraph = m_content.split("\n")[0].strip() if m_content else "Untitled"
+            page_html = page.inner_html("html")
+            soup = BeautifulSoup(page_html, 'html.parser')
+            title = soup.find('h5', class_='card-header')
+            title_text = title.get_text(strip=True) if title else "Untitled"
+
+            descriptions = [p.get_text(strip=True) for p in soup.find_all('p') if p.get_text(strip=True)]
+            combined_descriptions = " ".join(descriptions)
+
+            image_sources = [img['src'] for img in soup.find_all('img', src=True)]
+
+            download_button = soup.find('a', class_='app-academy-md-50 btn btn-label-primary d-flex align-items-center')
+            download_link = download_button['href'] if download_button and 'href' in download_button.attrs else None
+
 
 
             link_elements = page.query_selector_all('a[href]')
             web_links = [a.get_attribute('href') for a in link_elements]
 
             card_data = leak_model(
-                m_screenshot=helper_method.get_screenshot_base64(page, first_paragraph),
-                m_title=first_paragraph,
+                m_screenshot=helper_method.get_screenshot_base64(page, title_text),
+                m_title=title_text,
                 m_url=page.url,
                 m_base_url=self.base_url,
-                m_content=m_content + " " + self.base_url + " " + page.url,
-                m_important_content=m_content,
+                m_content=m_content,
+                m_important_content=combined_descriptions[:500],
                 m_network=helper_method.get_network_type(self.base_url),
                 m_weblink=web_links,
-                m_dumplink=[],
+                m_dumplink=[download_link],
                 m_content_type=["leaks"],
+                m_logo_or_images=image_sources,
             )
 
             entity_data = entity_model(
