3d51a77b32cfeddd24e71269f0322b3a6a86871c
عبدالمنان <msmannan00@protonmail.com>
2025-05-07 12:31:55 +0500

    bug fixes

--- leak_collector/scripts/_orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.py
+++ leak_collector/scripts/_orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.py
@@ -15,156 +15,153 @@ from crawler.crawler_services.shared.helper_method import helper_method
 
 
 class _orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id(leak_extractor_interface, ABC):
-    _instance = None
+  _instance = None
 
-    def __init__(self, callback=None):
-        self.callback = callback
-        self._card_data = []
-        self._entity_data = []
-        self.soup = None
-        self._initialized = None
-        self._redis_instance = redis_controller()
+  def __init__(self, callback=None):
+    self.callback = callback
+    self._card_data = []
+    self._entity_data = []
+    self.soup = None
+    self._initialized = None
+    self._redis_instance = redis_controller()
 
-    def init_callback(self, callback=None):
-        self.callback = callback
+  def init_callback(self, callback=None):
+    self.callback = callback
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(_orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id, cls).__new__(cls)
-            cls._instance._initialized = False
-        return cls._instance
+  def __new__(cls):
+    if cls._instance is None:
+      cls._instance = super(_orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id, cls).__new__(cls)
+      cls._instance._initialized = False
+    return cls._instance
 
-    @property
-    def seed_url(self) -> str:
-        return "http://orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.onion"
+  @property
+  def seed_url(self) -> str:
+    return "http://orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.onion"
 
-    @property
-    def base_url(self) -> str:
-        return "http://orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.onion"
+  @property
+  def base_url(self) -> str:
+    return "http://orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.onion"
 
-    @property
-    def rule_config(self) -> RuleModel:
-        return RuleModel(m_fetch_proxy=FetchProxy.TOR, m_fetch_config=FetchConfig.PLAYRIGHT)
+  @property
+  def rule_config(self) -> RuleModel:
+    return RuleModel(m_fetch_proxy=FetchProxy.TOR, m_fetch_config=FetchConfig.PLAYRIGHT)
 
-    @property
-    def card_data(self) -> List[leak_model]:
-        return self._card_data
+  @property
+  def card_data(self) -> List[leak_model]:
+    return self._card_data
 
-    @property
-    def entity_data(self) -> List[entity_model]:
-        return self._entity_data
+  @property
+  def entity_data(self) -> List[entity_model]:
+    return self._entity_data
 
-    def invoke_db(self, command: int, key: CUSTOM_SCRIPT_REDIS_KEYS, default_value):
-        return self._redis_instance.invoke_trigger(command, [key.value + self.__class__.__name__, default_value])
+  def invoke_db(self, command: int, key: CUSTOM_SCRIPT_REDIS_KEYS, default_value):
+    return self._redis_instance.invoke_trigger(command, [key.value + self.__class__.__name__, default_value])
 
-    def contact_page(self) -> str:
-        return "http://orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.onion"
+  def contact_page(self) -> str:
+    return "http://orca66hwnpciepupe5626k2ib6dds6zizjwuuashz67usjps2wehz4id.onion"
 
-    def append_leak_data(self, leak: leak_model, entity: entity_model):
-        self._card_data.append(leak)
-        self._entity_data.append(entity)
-        if self.callback:
-            if self.callback():
-                self._card_data.clear()
-                self._entity_data.clear()
+  def append_leak_data(self, leak: leak_model, entity: entity_model):
+    self._card_data.append(leak)
+    self._entity_data.append(entity)
+    if self.callback:
+      if self.callback():
+        self._card_data.clear()
+        self._entity_data.clear()
 
-    @staticmethod
-    def safe_find(page, selector, attr=None):
-        try:
-            element = page.query_selector(selector)
-            if element:
-                return element.get_attribute(attr) if attr else element.inner_text().strip()
-        except Exception:
-            return None
+  @staticmethod
+  def safe_find(page, selector, attr=None):
+    try:
+      element = page.query_selector(selector)
+      if element:
+        return element.get_attribute(attr) if attr else element.inner_text().strip()
+    except Exception:
+      return None
 
-    def parse_leak_data(self, page: Page):
-        try:
-            page.goto(self.seed_url)
-            page.wait_for_load_state('load')
+  def parse_leak_data(self, page: Page):
+    try:
+      page.goto(self.seed_url)
+      page.wait_for_load_state('load')
 
-            card_links = page.query_selector_all("a.blog__card-btn.--button")
-            if not card_links:
-                print("No card links found on the page.")
-                return
+      card_links = page.query_selector_all("a.blog__card-btn.--button")
+      if not card_links:
+        print("No card links found on the page.")
+        return
 
-            card_urls = [urljoin(self.base_url, link.get_attribute("href")) for link in card_links]
+      card_urls = [urljoin(self.base_url, link.get_attribute("href")) for link in card_links]
 
-            for card_url in card_urls:
-                page.goto(card_url)
-                page.wait_for_load_state('load')
+      for card_url in card_urls:
+        page.goto(card_url)
+        page.wait_for_load_state('load')
 
-                page_html = page.content()
-                self.soup = BeautifulSoup(page_html, 'html.parser')
+        page_html = page.content()
+        self.soup = BeautifulSoup(page_html, 'html.parser')
 
-                card_inner = self.soup.select_one("div.card__inner")
-                if not card_inner:
-                    print(f"No card inner found on the page: {card_url}")
-                    continue
+        card_inner = self.soup.select_one("div.card__inner")
+        if not card_inner:
+          print(f"No card inner found on the page: {card_url}")
+          continue
 
-                description = self.safe_find(page, "div.card__description-content", attr=None)
-                
-                company_url = self.safe_find(page, "a.card__info-text.--card__info-text-link", attr="href")
-                download_url = self.safe_find(page, "a.card__download.--button", attr="href")
-                card_title = self.safe_find(page, "h1.card__title", attr=None)
+        description = self.safe_find(page, "div.card__description-content", attr=None)
 
+        company_url = self.safe_find(page, "a.card__info-text.--card__info-text-link", attr="href")
+        download_url = self.safe_find(page, "a.card__download.--button", attr="href")
+        card_title = self.safe_find(page, "h1.card__title", attr=None)
 
-                image_logos = []
-                card_photos_images = self.soup.select("img.card__photos-img")
-                for img in card_photos_images:
-                    if img.has_attr("src"):
-                        image_logos.append(img["src"])
+        image_logos = []
+        card_photos_images = self.soup.select("img.card__photos-img")
+        for img in card_photos_images:
+          if img.has_attr("src"):
+            image_logos.append(img["src"])
 
-                number_of_files = None
-                date_of_publication = None
-                data_size = None
+        date_of_publication = None
+        data_size = None
 
-                info_items = card_inner.select("div.card__info-item")
-                for item in info_items:
-                    title_elem = item.select_one("h2.card__info-item-title")
-                    if not title_elem:
-                        continue
+        info_items = card_inner.select("div.card__info-item")
+        for item in info_items:
+          title_elem = item.select_one("h2.card__info-item-title")
+          value_elem = item.select_one("p.card__info-text")
+          if not title_elem or not value_elem:
+            continue
 
-                    title_text = title_elem.get_text(strip=True).lower()
-                    if "date of publication" in title_text:
-                        date_elem = item.select_one("p.card__info-text")
-                        if date_elem:
-                            date_of_publication = date_elem.get_text(strip=True)
-                    elif "number of files" in title_text:
-                        number_elem = item.select_one("p.card__info-text")
-                        if number_elem:
-                            number_of_files = number_elem.get_text(strip=True)
-                    elif "files size" in title_text:
-                        size_elem = item.select_one("p.card__info-text")
-                        if size_elem:
-                            data_size = size_elem.get_text(strip=True)
+          title_text = title_elem.get_text(strip=True).lower()
+          value_text = value_elem.get_text(strip=True)
 
-                if date_of_publication is None:
-                    date_of_publication = ""
+          if "open in" in title_text:
+            date_of_publication = value_text
+          elif "files size" in title_text:
+            data_size = value_text
 
-                card_data = leak_model(
-                    m_screenshot=helper_method.get_screenshot_base64(page, card_title),
-                    m_title=card_title,
-                    m_url=self.base_url,
-                    m_weblink=[company_url] if company_url else [],
-                    m_dumplink=[download_url],
-                    m_network=helper_method.get_network_type(self.base_url),
-                    m_base_url=self.base_url,
-                    m_content=description + " " + self.base_url + " " + page.url,
-                    m_important_content=description[:500],
-                    m_content_type=["leaks"],
-                    m_data_size=data_size,
-                    m_logo_or_images=image_logos,
-                    m_leak_date=datetime.datetime.strptime(date_of_publication,'%d/%m/%Y').date() if date_of_publication else None,
-                    m_records_size=number_of_files
-                )
+        if date_of_publication is None:
+          date_of_publication = ""
 
-                entity_data = entity_model(
-                    m_email_addresses=helper_method.extract_emails(description) if description else [],
-                    m_phone_numbers=helper_method.extract_phone_numbers(description) if description else [],
-                    m_company_name=card_title,
-                )
+        if not company_url:
+          company_url = ""
 
-                self.append_leak_data(card_data, entity_data)
+        card_data = leak_model(
+          m_screenshot=helper_method.get_screenshot_base64(page, card_title),
+          m_title=card_title,
+          m_url=self.base_url,
+          m_weblink=[company_url] if company_url else [],
+          m_dumplink=[download_url],
+          m_network=helper_method.get_network_type(self.base_url),
+          m_base_url=self.base_url,
+          m_content=description + " " + self.base_url + " " + page.url,
+          m_important_content=description[:500],
+          m_content_type=["leaks"],
+          m_data_size=data_size,
+          m_leak_date=datetime.datetime.strptime(date_of_publication,
+                                                 '%d/%m/%Y').date() if date_of_publication else None,
+        )
 
-        except Exception as ex:
-            print(f"An error occurred: {ex}")
+        entity_data = entity_model(
+          m_email_addresses=helper_method.extract_emails(description) if description else [],
+          m_phone_numbers=helper_method.extract_phone_numbers(description) if description else [],
+          m_company_name=card_title,
+          m_ip=[company_url],
+          m_team="public ocra"
+        )
+
+        self.append_leak_data(card_data, entity_data)
+
+    except Exception as ex:
+      print(f"An error occurred: {ex}")
